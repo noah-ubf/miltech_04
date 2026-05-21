d871ab90f5c18dbecc0d288b64e53dbbc3c6a0df
Oleksandr Zaikin <nuh.ubf@gmail.com>
2026-05-21 09:24:11 +0000

    CMakeList updated & code cleaning

--- homework_06/src/main.cpp
+++ homework_06/src/main.cpp
@@ -1,15 +1,18 @@
-#include <cstring>
-#include <cmath>
-#include "../include/ballistics.hpp";
+#include <iostream>
 
-using namespace std;
+#include "../include/ballistics.hpp"
 
 /*
   Fire point and maneuver calculation for a drone.
 */
-int main()
+int main(int argc, char** argv)
 {
-    Config config = readConfig("input.txt");
+    if(argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <input_file>" << std::endl;
+        return 1;
+    }
+
+    Config config = readConfig(argv[1]);
     if (!config.isValid) {
         return 1;
     }
